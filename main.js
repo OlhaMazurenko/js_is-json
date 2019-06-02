c7eb8fd5d25dd51172940fe6a63d279685d3b18d
OlhaMazurenko <mail.olgamazurenko@gmail.com>
2019-06-02 19:53:36 +0300

    'Solution'

--- main.js
+++ main.js
@@ -7,8 +7,7 @@ function isJSON(str) {
     }
 }
 
-
 isJSON('{"name": "Michael", "age": 45}'); // true
 isJSON('{name: "Michael", age: 45}'); // false
 isJSON('false'); // true
-isJSON('abc'); // false 
+isJSON('abc'); // false
